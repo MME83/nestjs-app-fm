de9708d26ae0777cb62f1294b5ec4979692985fb
Mykola Moroz <moroz.mykola@gmail.com>
2023-01-16 00:49:46 +0200

    nest-fm: add config swagger to the main

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -8,6 +9,16 @@ async function bootstrap() {
   const port = process.env.PORT;
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Finance manager API')
+    .setDescription('API recives bank transactions and shows stat')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
 
   logger.log(`Application listening on port ${port}`);
