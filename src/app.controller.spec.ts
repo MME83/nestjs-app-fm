0dcb7f3b1c9d27e89682d36823c227d9418911ee
Mykola Moroz <moroz.mykola@gmail.com>
2023-01-16 00:35:34 +0200

    nest-fm: add health check end-point to api

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { HttpStatus } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -15,8 +16,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "200"', () => {
+      expect(appController.root()).toBe(HttpStatus.OK);
     });
   });
 });
